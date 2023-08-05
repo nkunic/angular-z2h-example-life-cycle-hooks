8161aa3204b419e47517fe9d6d10163fb0f80ad1
nkunic <45877400+nkunic@users.noreply.github.com>
2023-08-05 22:46:26 +0200

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, ParentComponent, ChildComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
